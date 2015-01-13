b2962b13314f43559b84ae9ef093cfa557154060
L McGuire <louise.h.mcguire@gmail.com>
2015-01-13 10:07:01 +0100

    Final

--- js/app.js
+++ js/app.js
@@ -1,20 +1,30 @@
 $(document).ready(function() {
 	console.log('ready');
 
-	$('textarea').keypress(function(e) {
+	$('input').keyup(function(e) {
 
-		var addItem = $('textarea').val();
-		var trashIcon = "<li class='trash-icon'><i class='fa fa-trash-o fa-3x'></i></li>";
-		var checkIcon = "<li class='check-icon'><i class='fa fa-check-square fa-3x'></i></li>";
-
+		var addItem = $('input').val();
+		var newItem = "<li><span class='fa fa-trash-o fa-3x'></span><span class='item'>" + addItem + "</span><span class='fa fa-check-square fa-3x'></span></li>";
+		
 		if(e.keyCode == 13) {
-			$('ul').append(trashIcon);
-			$('ul').append(addItem);
-			$('ul').append(checkIcon);
-			
+				if($.trim(addItem) == '') {
+					alert('Plese Enter an Item!');
+				} else {
+
+			$('ul').append(newItem);
+			$('input').val('');
+				}
 		}
-	
 	});
 
+	$('ul').on('click', '.fa-check-square', function () {
+		$(this).parent().toggleClass('selected');
+	});
 
+	$('ul').on('click', '.fa-trash-o', function () {
+		$(this).parent().fadeOut(500, function () {
+			$(this).remove();
+
+		})
+	});
 })
